2dcfcfb5faf0e30ccb9d5ed565de31e2703da584
Dang <huynhminhdang@gmail.com>
2022-11-22 08:15:15 +0700

    Update dpkg.fish

--- functions/dpkg.fish
+++ functions/dpkg.fish
@@ -23,13 +23,13 @@ function _dpkg_install_deb -a url
   set -l dir (mktemp -d)
   if not wget -q --show-progress -P $dir $url
     echo failed to download deb package $url
-    exit 1
+    return 1
   end
   set -l f $dir/*
   # extract
   if not command dpkg -x $f $dir/local
     echo failed to extract deb package $f
-    exit 1
+    return 1
   end
   # install
   _dpkg_install_dir $dir/local
@@ -40,13 +40,13 @@ function _dpkg_install_tarball -a url
   set -l dir (mktemp -d)
   if not wget -q --show-progress -P $dir $url
     echo failed to download tarball $url
-    exit 1
+    return 1
   end
   set -l f $dir/*
   # extract
   if not command tar -xvf $f -C $dir/local
     echo failed to extract tarball $f
-    exit 1
+    return 1
   end
   # install
   _dpkg_install_dir $dir/local
@@ -57,7 +57,7 @@ function _dpkg_install_appimage -a url
   set -l dir (mktemp -d)
   if not wget -q --show-progress -P $dir $url
     echo failed to download app image $url
-    exit 1
+    return 1
   end
   set -l f $dir/*
   # extract
@@ -67,19 +67,19 @@ function _dpkg_install_appimage -a url
     $f --appimage-extract
   " > /dev/null
     echo failed to extract app image $f
-    exit 1
+    return 1
   end
   # install
   _dpkg_install_dir $dir/squashfs-root/usr
 end
 
 function _dpkg_install_dir -a dir
-  test -d $dir || exit 1
+  test -d $dir || return 1
   # normalize paths
   if test -d $dir/usr
-    rsync -avg $dir/usr $dir
+    rsync -avu $dir/usr/ $dir
     rm -r $dir/usr
-  end
+  end > /dev/null
   # get package name
   set -l pkg
   if test -d $dir/bin
@@ -91,22 +91,16 @@ function _dpkg_install_dir -a dir
   end
   if test -z "$pkg"
     echo unknown package name!
-    exit 1
+    return 1
   end
-  # keep track of package files
-  mkdir -p $HOME/.config/dpkg
-  fish -c "
-    cd $dir
-    find . -type f | sed -e 's/^\.\///' > $HOME/.config/dpkg/$pkg
-  "
   # install to home dir
-  rsync -avuI $dir/ $HOME/.local/
+  rsync -avIi $dir/ $HOME/.local/ | grep '>f' | cut -d' ' -f2- | tee $HOME/.config/dpkg/$pkg
 end
 
 function _dpkg_remove -a pkg
   if not test -f $HOME/.config/dpkg/$pkg
     echo package $pkg does not exist!
-    exit 1
+    return 1
   end
   # remove package files
   while read -z -l f
