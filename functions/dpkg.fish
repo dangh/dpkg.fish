cf804a8c9a9aadcb424c72d08912e6e89d5b2217
Dang <huynhminhdang@gmail.com>
2022-11-22 07:48:49 +0700

    Update dpkg.fish

--- functions/dpkg.fish
+++ functions/dpkg.fish
@@ -18,30 +18,10 @@ function dpkg -d 'install package without root'
   end
 end
 
-function _dpkg_install
-  set -l packages $argv
-  set -l tmpdir (mktemp -d)
-  # download packages
-  mkdir -p $tmpdir/src
-  wget -P $tmpdir/src $packages
-  # extract packages
-  mkdir $tmpdir/extract
-  for f in $tmpdir/src/*
-    switch $f
-      case '*.deb'
-        _dpkg_install_deb $f
-      case '*.tar.gz'
-        _dpkg_install_tar $f
-      case '*.AppImage'
-        _dpkg_install_appimage $f
-    end
-  end
-end
-
 function _dpkg_install_deb -a url
   # fetch
   set -l dir (mktemp -d)
-  wget -P $dir $url
+  wget -q --show-progress -P $dir $url || exit 1
   set -l f $dir/*
   # extract
   command dpkg -x $f $dir/local
@@ -52,7 +32,7 @@ end
 function _dpkg_install_tarball -a url
   # fetch
   set -l dir (mktemp -d)
-  wget -P $dir $url
+  wget -q --show-progress -P $dir $url || exit 1
   set -l f $dir/*
   # extract
   command tar -xvf $f -C $dir/local
@@ -63,49 +43,59 @@ end
 function _dpkg_install_appimage -a url
   # fetch
   set -l dir (mktemp -d)
-  wget -P $dir $url
+  wget -q --show-progress -P $dir $url || exit 1
   set -l f $dir/*
   # extract
   fish -c "
     chmod +x $f
     cd $dir
     $f --appimage-extract
-  "
+  " > /dev/null
   # install
   _dpkg_install_dir $dir/squashfs-root/usr
 end
 
 function _dpkg_install_dir -a dir
+  test -d $dir || exit 1
   # normalize paths
   if test -d $dir/usr
     rsync -avg $dir/usr $dir
     rm -r $dir/usr
   end
-  # install to home dir
-  rsync -avu $dir/ $HOME/.local/
   # get package name
   set -l pkg
   if test -d $dir/bin
     for f in $dir/bin/*
-      if test (string length (path basename $f)) -lt (string length "$pkg")
+      if test -z "$pkg" -o (string length (path basename $f)) -lt (string length "$pkg")
         set pkg (path basename $f)
       end
     end
   end
-  # keep track of install files
-  if test -n "$pkg"
-    mkdir -p $HOME/.config/dpkg
-    fish -c "
-      cd $dir
-      find . -type d | sed -e 's/.\///' > $HOME/.config/dpkg/$pkg
-    "
+  if test -z "$pkg"
+    echo unknown package name!
+    exit 1
   end
+  # keep track of package files
+  mkdir -p $HOME/.config/dpkg
+  fish -c "
+    cd $dir
+    find . -type f | sed -e 's/^\.\///' > $HOME/.config/dpkg/$pkg
+  "
+  # install to home dir
+  rsync -avuI $dir/ $HOME/.local/
 end
 
 function _dpkg_remove -a pkg
-  test -f $HOME/.config/dpkg/$pkg || exit 1
+  if ! test -f $HOME/.config/dpkg/$pkg
+    echo package $pkg does not exist!
+    exit 1
+  end
+  # remove package files
   while read -z -l f
-    rm -f $HOME/.local/$f
+    rm -vf $HOME/.local/$f
   end < $HOME/.config/dpkg/$pkg
+  # remove track file
   rm $HOME/.config/dpkg/$pkg
+  # cleanup empty dirs
+  find $HOME/.local -type d -empty -delete
 end
